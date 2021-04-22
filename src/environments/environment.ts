a27df2bf393b8be60d643c5a98bbc2e9e81196ff
Daniel2001AL <193290@ids.upchiapas.edu.mx>
2021-04-21 20:41:40 -0500

    Create login view

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAe9sjWTgCZHSlnqesHOZloF80O85F6-kc",
+    authDomain: "poli-elec2021.firebaseapp.com",
+    projectId: "poli-elec2021",
+    storageBucket: "poli-elec2021.appspot.com",
+    messagingSenderId: "55404074087",
+    appId: "1:55404074087:web:ab2693677e17dcc525b0c2",
+    measurementId: "G-DBRF69S6FE"
+  }
 };
 
 /*
